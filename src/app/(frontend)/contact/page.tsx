0820aa3d343c3fcc951f3086385e7a3f41e5b217
codenikin <codenikin@gmail.com>
2025-12-07 00:04:23 +0530

    new

--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -1,9 +1,10 @@
 import ContactForm from '@/components/ContactSection'
-
+import TeamInner from '@/components/TeamInner/TeamInner'
 export const dynamic = 'force-dynamic'
 export default async function ContactPage() {
   return (
     <>
+      <TeamInner />
       <ContactForm />
     </>
   )
