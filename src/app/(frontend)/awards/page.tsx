638a079ff059c4c807ce77aa5cc0302f936f5bb6
codenikin <codenikin@gmail.com>
2025-12-06 20:33:47 +0530

    new

--- src/app/(frontend)/awards/page.tsx
+++ src/app/(frontend)/awards/page.tsx
@@ -1,5 +1,4 @@
 import { Certificate } from '@/components/Certificate'
-import Certificates from '@/components/CertificateZoom'
 import FeatureSection from '@/components/FeatureSection'
 export const dynamic = 'force-dynamic'
 export default async function AwardPage() {
